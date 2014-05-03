5234010c9ea32c176215fe812b90aaf41dceddc1
Aaron Bruce <aarondbruce@gmail.com>
2014-05-02 23:43:14 -0700

    switched apache user to vagrant user

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -3,7 +3,9 @@
 apt-get update
 apt-get install -y apache2
 rm -rf /var/www
-ln -fs /vagrant /var/www
+ln -fs /vagrant/htdocs /var/www
+sudo sed -i 's/www-data/vagrant/g' /etc/apache2/envvars
+chown -R vagrant:www-data /var/lock/apache2
 
 #install php
 apt-get install -y php5 libapache2-mod-php5
@@ -20,7 +22,7 @@ mysql -u root --password='' -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTI
 sudo service mysql restart
 
 #install php modules
-apt-get -y install php5-mysql php5-mcrypt
+apt-get -y install php5-mysql php5-mcrypt php5-curl
 
 #restart apache
 service apache2 restart
