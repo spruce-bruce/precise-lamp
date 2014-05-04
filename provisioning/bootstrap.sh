60cb8a2113ff287afb80d4763f16cc30de662bc3
Aaron Bruce <aarondbruce@gmail.com>
2014-05-04 13:01:37 -0700

    added mod rewrite and php GD

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Install Apache
 apt-get update
 apt-get install -y apache2
 rm -rf /var/www
@@ -7,13 +8,17 @@ ln -fs /vagrant/htdocs /var/www
 sudo sed -i 's/www-data/vagrant/g' /etc/apache2/envvars
 chown -R vagrant:www-data /var/lock/apache2
 
-#install php
+a2enmod rewrite
+sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/sites-available/default
+
+
+# Install php
 apt-get install -y php5 libapache2-mod-php5
 
-#install vim
+# Install vim
 apt-get -y install vim
 
-#install mysql
+# Install mysql
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server
 
@@ -21,8 +26,8 @@ sudo sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/my
 mysql -u root --password='' -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 sudo service mysql restart
 
-#install php modules
-apt-get -y install php5-mysql php5-mcrypt php5-curl
+# Install php modules
+apt-get -y install php5-mysql php5-mcrypt php5-curl php5-gd
 
-#restart apache
+# Restart apache
 service apache2 restart
